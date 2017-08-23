3297aa243e2088ba34ec4f23849d899a3cc1ce59
Angel M <angel@laux.es>
2017-08-24 00:21:52 +0200

    Store the data with nedb

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const NeDB = require('nedb');
+const NeDB = require('nedb-promise');
 const {
   micro,
   json,
@@ -7,6 +7,7 @@ const {
 } = require('micro');
 const cors = require('micro-cors');
 const parseDomain = require('parse-domain');
+const url = require('url');
 
 // Constants
 const ENV = process.env.NODE_ENV || 'development';
@@ -22,9 +23,15 @@ const db = new NeDB({
  * handle POST requests
  */
 async function postHandler(request, clapUrl) {
+  let res = await db.update(
+    { host: clapUrl.host, path: clapUrl.path },
+    { $inc: { count: 1 } },
+    { upsert: true, returnUpdatedDocs: true }
+  );
+
   return {
-    clap: true,
-    count: 1000
+    clap: res[0] === 1,
+    count: res[1].count
   }
 }
 
@@ -32,22 +39,26 @@ async function postHandler(request, clapUrl) {
  * handle GET requests
  */
 async function getHandler(request, clapUrl) {
-  return {
-    count: 1000
-  };
+  let docs = await db.find({ host: clapUrl.host, path: clapUrl.path });
+  if (docs.length === 0) {
+    return { count: 0 };
+  } else {
+    return { count: docs[0].count }
+  }
 }
 
 // This method will process the requests
 async function methodHandler(request, response) {
   // Extract common data
-  const originUrl = parseDomain(request.headers.origin || '');
-  const clapUrl = parseDomain(request.url.slice(1) || '');
+  const originDomain = parseDomain(request.headers.origin || '');
+  const clapDomain = parseDomain(request.url.slice(1) || '');
+  const clapUrl = url.parse(request.url.slice(1) || '');
 
-  if (!clapUrl) {
+  if (!clapDomain) {
     return send(response, 401, { clap: false, error: "The URL to clap is not valid" });
   }
 
-  if (!originUrl || originUrl.domain !== clapUrl.domain || originUrl.tld !== clapUrl.tld) {
+  if (!originDomain || originDomain.domain !== clapDomain.domain || originDomain.tld !== clapDomain.tld) {
     return send(response, 401, { clap: false, error: "Send requests only from the same domain" });
   }
 
